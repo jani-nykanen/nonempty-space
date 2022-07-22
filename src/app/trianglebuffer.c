69605e3627750ae41f541d2ea61f232ff02b25bd
jani-nykanen <jnyknn@gmail.com>
2022-07-22 11:19:27 +0300

    More 3D renderer stuff

--- src/app/trianglebuffer.c
+++ src/app/trianglebuffer.c
@@ -12,10 +12,28 @@ static i32 compare_triangles(const void* a, const void* b) {
 }
 
 
+static Vector4 project(Vector4 P) {
+
+    Vector4 out;
+
+    out.x = P.x / P.z;
+    out.y = P.y / P.z;
+
+    out.x += 1.0f;
+    out.y += 1.0f;
+
+    out.x /= 2.0f;
+    out.y /= 2.0f;
+
+    return out;
+}
+
+
 Triangle create_triangle(
-    i32 x1, i32 y1, i32 x2, i32 y2, i32 x3, i32 y3,
+    f32 x1, f32 y1, f32 x2, f32 y2, f32 x3, f32 y3,
     f32 u1, f32 v1, f32 u2, f32 v2, f32 u3, f32 v3,
-    Bitmap* texture, i32 tint, f32 depth) {
+    Bitmap* texture, u8 color,
+    i32 tint, f32 depth) {
 
     // Yes, I know {Triangle} {x1, y1, ..., } exists,
     // but some compilers do not support it.
@@ -37,10 +55,46 @@ Triangle create_triangle(
     t.v3 = v3;
 
     t.texture = texture;
+    t.color = color;
     t.tint = tint;
     t.depth = depth;
 
     return t;
+}
+
+
+bool create_triangle_3D(
+    Bitmap* texture, u8 color,
+    Transformations* transf,
+    Vector4 A, Vector4 B, Vector4 C,
+    Vector4 tA, Vector4 tB, Vector4 tC,
+    Vector4 normal,
+    Triangle* dest) {
+
+    const f32 NEAR = 0.025f;
+
+    Vector4 pA, pB, pC;
+    f32 depth;
+    
+    A = transf_apply_to_vector(transf, A);
+    B = transf_apply_to_vector(transf, B);
+    C = transf_apply_to_vector(transf, C);
+
+    if (A.z < NEAR || B.z < NEAR || C.z < NEAR)
+        return false;
+
+    depth = (A.z + B.z + C.z) / 3.0f;
+
+    pA = project(A);
+    pB = project(B);
+    pC = project(C);
+
+    *dest = create_triangle(
+        pA.x, pA.y, pB.x, pB.y, pC.x, pC.y,
+        tA.x, tA.y, tB.x, tB.y, tC.x, tC.y,
+        texture, color, 0, depth);
+
+    return true;
 }
 
 
@@ -104,6 +158,12 @@ bool tribuf_push_triangle(TriangleBuffer* buf, Triangle triangle) {
 void tribuf_draw(TriangleBuffer* buf, TriangleRasterizer* tri) {
 
     i32 i;
+    Canvas* canvas = tri->canvas;
+    Triangle t;
+    i32 x1, y1, x2, y2, x3, y3;
+
+    f32 w = (f32) canvas->width;
+    f32 h = (f32) canvas->height;
 
     if (buf->count == 0) return;
 
@@ -111,7 +171,15 @@ void tribuf_draw(TriangleBuffer* buf, TriangleRasterizer* tri) {
 
     for (i = 0; i < buf->count; ++ i) {
 
-        // ...
+        t = buf->triangles[i];
+
+        tri_set_uv_coordinates(tri, t.u1, t.v1, t.u2, t.v2, t.u3, t.v3);
+
+        x1 = (i32) (t.x1 * w); y1 = (i32) (t.y1 * h);
+        x2 = (i32) (t.x2 * w); y2 = (i32) (t.y2 * h);
+        x3 = (i32) (t.x3 * w); y3 = (i32) (t.y3 * h);
+
+        tri_draw_triangle(tri, t.texture, t.color, x1, y1, x2, y2, x3, y3);
     }
 
     tribuf_flush(buf);
--- src/app/trianglebuffer.h
+++ src/app/trianglebuffer.h
@@ -4,16 +4,17 @@
 
 #include "common/vector.h"
 
-
 #include "bitmap.h"
 #include "rasterizer.h"
+#include "transformations.h"
 
 
 typedef struct {
 
-    i32 x1, y1, x2, y2, x3, y3;
+    f32 x1, y1, x2, y2, x3, y3;
     f32 u1, v1, u2, v2, u3, v3;
     Bitmap* texture;
+    u8 color;
     i32 tint;
     f32 depth;
 
@@ -30,9 +31,17 @@ typedef struct {
 
 
 Triangle create_triangle(
-    i32 x1, i32 y1, i32 x2, i32 y2, i32 x3, i32 y3,
+    f32 x1, f32 y1, f32 x2, f32 y2, f32 x3, f32 y3,
     f32 u1, f32 v1, f32 u2, f32 v2, f32 u3, f32 v3,
-    Bitmap* texture, i32 tint, f32 depth);
+    Bitmap* texture, u8 color,
+    i32 tint, f32 depth);
+bool create_triangle_3D(
+    Bitmap* texture, u8 color,
+    Transformations* transf,
+    Vector4 A, Vector4 B, Vector4 C,
+    Vector4 tA, Vector4 tB, Vector4 tC,
+    Vector4 normal,
+    Triangle* dest);
 
 
 TriangleBuffer* new_triangle_buffer(u32 maxSize, Error* err);
